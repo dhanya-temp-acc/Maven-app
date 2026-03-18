1b16810bc0baa84504288521193eed065f497ccb
dhanya-17232 <dhanya.pj@zohocorp.com>
2026-03-18 13:08:18 +0530

    Added basic maven plugin project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println("Sum2 = " + main.add(2,3));
+        System.out.println("Mean2 = " + (main.add(2,3))/2);
+    }
+}
